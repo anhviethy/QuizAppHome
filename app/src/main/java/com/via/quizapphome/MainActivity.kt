b1d83f12084dc2f70cfcaaf9425602edb8d66dd1
via <anhviethy@gmail.com>
2022-12-05 23:01:17 +0700

    [11:00PM - 05122022] Add QuestionActivity and start activity from main activity

--- app/src/main/java/com/via/quizapphome/MainActivity.kt
+++ app/src/main/java/com/via/quizapphome/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.via.quizapphome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var btnStart: Button? = null
+    var etName: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btn_start)
+        etName = findViewById(R.id.et_name)
+        btnStart?.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            if (etName!!.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name!", Toast.LENGTH_LONG).show()
+            } else {
+                startActivity(intent)
+            }
+        }
+
     }
 }
