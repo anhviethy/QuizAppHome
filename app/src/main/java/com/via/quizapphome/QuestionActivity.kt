aeac04a5f1c8aaf0da22fe34b2590d70b5e07b69
via <anhviethy@gmail.com>
2022-12-07 23:02:32 +0700

    [11:01PM - 07122022] Add view for border when option is selected

--- app/src/main/java/com/via/quizapphome/QuestionActivity.kt
+++ app/src/main/java/com/via/quizapphome/QuestionActivity.kt
@@ -1,9 +1,11 @@
 package com.via.quizapphome
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -16,20 +18,27 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener {
     var mTvOption2: TextView? = null
     var mTvOption3: TextView? = null
     var mTvOption4: TextView? = null
+    var mIvFlag: ImageView? = null
     var mBtnSubmit: Button? = null
 
     var mSelectedOption: Int = 0
-    var mCurrentIdQuestion: Int = 0
+    var mCurrentIdQuestion: Int = 1
     var mListQuestion: ArrayList<Question>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+        mListQuestion = Constants.getQuestion()
         getViewById()
+        setQuestionInfo()
         setDefaultOptionView()
+        mTvOption1?.setOnClickListener(this)
+        mTvOption2?.setOnClickListener(this)
+        mTvOption3?.setOnClickListener(this)
+        mTvOption4?.setOnClickListener(this)
     }
 
-    fun setDefaultOptionView() {
+    private fun setDefaultOptionView() {
         var listOption: ArrayList<TextView> = ArrayList()
         mTvOption1?.let { listOption.add(it) }
         mTvOption2?.let { listOption.add(it) }
@@ -42,17 +51,35 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    fun setQuestionInfo(){
+    private fun setQuestionInfo() {
+        val curQuestion: Question = mListQuestion!!.get(mCurrentIdQuestion - 1)
+        mIvFlag?.setImageResource(curQuestion.flag)
+        mProgressBar?.progress = mCurrentIdQuestion
+        mTvProgressBar?.text = "$mCurrentIdQuestion/${mListQuestion!!.size}"
+        mTvOption1?.text = curQuestion.option1
+        mTvOption2?.text = curQuestion.option2
+        mTvOption3?.text = curQuestion.option3
+        mTvOption4?.text = curQuestion.option4
 
+        if (mCurrentIdQuestion == mListQuestion!!.size) {
+            mBtnSubmit?.text = "FINNISH"
+        } else {
+            mBtnSubmit?.text = "SUBMIT"
+        }
     }
 
-    fun setSelectedOptionView(optionSelected: Int){
-
+    private fun setSelectedOptionView(optionSelected: Int, viewOption: TextView) {
+        setDefaultOptionView()
+        mSelectedOption = optionSelected
+        viewOption.setTextColor(getColor(R.color.color_text_option))
+        viewOption.setTypeface(viewOption.typeface, Typeface.BOLD)
+        viewOption.background = ContextCompat.getDrawable(this, R.drawable.boder_selected_option)
     }
 
-    fun getViewById() {
+    private fun getViewById() {
         mProgressBar = findViewById(R.id.progress_horizontal)
         mTvProgressBar = findViewById(R.id.tv_progressbar)
+        mIvFlag = findViewById(R.id.iv_flag)
         mTvOption1 = findViewById(R.id.tv_option1)
         mTvOption2 = findViewById(R.id.tv_option2)
         mTvOption3 = findViewById(R.id.tv_option3)
@@ -62,7 +89,21 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v?.id) {
+            R.id.tv_option1 -> {
+                mTvOption1?.let { setSelectedOptionView(1, it) }
+            }
 
+            R.id.tv_option2 -> {
+                mTvOption2?.let { setSelectedOptionView(2, it) }
+            }
+
+            R.id.tv_option3 -> {
+                mTvOption3?.let { setSelectedOptionView(3, it) }
+            }
+
+            R.id.tv_option4 -> {
+                mTvOption4?.let { setSelectedOptionView(4, it) }
+            }
         }
     }
 }
